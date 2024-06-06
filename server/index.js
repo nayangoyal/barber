e284a6c7e91b8b90ed3e186b50b8b86839d0fd0c
nayangoyal <101893656+nayangoyal@users.noreply.github.com>
2024-06-07 01:10:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ mongoose.connect("mongodb+srv://dig:ab@barber.it6z4k9.mongodb.net/?retryWrites=t
 
 const verifyUser = (req, res, next) => {
   const token = req.cookies.token;
-  console.log(token);
+  console.log(req.cookies.token);
   if (!token) {
     return res.json("Token is missing");
   }
@@ -134,7 +134,7 @@ app.post('/Login', (req, res) => {
               maxAge: 86400000
             });
 
-            console.log(token);
+            console.log(req.cookies.token);
             return res.json(user);
           } else {
             return res.json("The password is incorrect");
