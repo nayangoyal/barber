cbbb4fd0ed28d0ade7a38894e0616ab5cce63e40
nayangoyal <101893656+nayangoyal@users.noreply.github.com>
2024-06-21 18:52:02 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,11 +17,13 @@ app.use(cookieParser());
 //   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 // };
 
-app.use(cors({
-  origin: 'https://barber-1flp.onrender.com',
-   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+// {
+//   origin: 'https://barber-1flp.onrender.com',
+//    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true
+// }
+
+app.use(cors());
 
 // Handle preflight requests
 // app.options('*', cors(corsOptions));
